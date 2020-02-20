94d8aa43d04b22df64de521e38b2d81d448822f7
Janelle Buchanan <janellebuch@gmail.com>
2020-02-20 17:48:10 -0600

    added new parser and tool files

---
+++ parser/log-parser/log-parser/log-parser.cpp
@@ -0,0 +1,12 @@
+// log-parser.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "parser.h"
+
+using namespace std;
+
+// This function contains the grammer definition
+extern int yyparse();
+
+
